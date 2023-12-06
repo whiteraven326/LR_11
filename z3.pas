ec9c71791c1846d854c175404b67c90048b7cdb8
whiteraven326 <144677455+whiteraven326@users.noreply.github.com>
2023-12-06 23:16:06 +0300

    Update z3.pas

--- z3.pas
+++ z3.pas
@@ -1,14 +1,15 @@
-﻿program z3;
+program z3;
 uses GraphABC;
+var x: integer;
 begin
+x:= 40;
+  
+repeat
+SetWindowHeight(350);
+ SetWindowWidth(700);
  setbrushcolor(clrandom);
-circle(50,100,10);
-circle(80,100,10);
-circle(110,100,10);
-circle(140,100,10);
-circle(170,100,10);
-circle(200,100,10);
-circle(230,100,10);
-circle(260,100,10);
-circle(290,100,10);
+circle(x,100,10);
+x:= x + 40;  
+ circle(x,100,10);   
+until x>300                               
 end.
